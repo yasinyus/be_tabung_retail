2effb2a8e51533757bd600a071f5a37fa07a9ced
Yasin Yusuf <yasinyus@users.noreply.github.com>
2025-10-13 19:13:39 +0700

    fix error

--- app/Filament/Resources/VolumeTabungResource.php
+++ app/Filament/Resources/VolumeTabungResource.php
@@ -51,8 +51,9 @@ class VolumeTabungResource extends Resource
             'create' => Pages\CreateVolumeTabung::route('/create'),
             'view' => Pages\ViewVolumeTabung::route('/{record}'),
             'edit' => Pages\EditVolumeTabung::route('/{record}/edit'),
-            'list-gudang' => Pages\ListGudang::route('/list-gudang'),
-            'gudang-detail' => Pages\DetailGudang::route('/gudang/{gudang}'),
+            // Temporarily removed due to file corruption issues
+            // 'list-gudang' => Pages\ListGudang::route('/list-gudang'),
+            // 'gudang-detail' => Pages\DetailGudang::route('/gudang/{gudang}'),
         ];
     }
 
